2bf7c85a7ac8aa2b9ba976b5b4ae57ed02848683
Mahabub Ali <alimahabub977@gmail.com>
2026-01-31 11:20:04 +0530

    Solved

--- String/StringPalindrome.java
+++ String/StringPalindrome.java
@@ -1,5 +1,16 @@
 package String;
 
 public class StringPalindrome {
-    
+    public static boolean isPalindrome(String str){
+        for(int i =0; i<str.length()/2; i++){
+            if(str.charAt(i)!=str.charAt(str.length()-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        String str = "racecar";
+        System.out.println(isPalindrome(str));
+    }
 }
