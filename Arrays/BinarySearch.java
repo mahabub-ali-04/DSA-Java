7cf230a21d6c78d839f1d67b7a097cb6ac0fd656
Mahabub Ali <alimahabub977@gmail.com>
2026-01-31 14:42:40 +0530

    solved

---
+++ Arrays/BinarySearch.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+public class BinarySearch {
+    public static int binarySearch(int numbers[], int key) {
+
+        int start = 0;
+        int end = numbers.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (numbers[mid] == key) {
+                return mid;
+            }
+            if (numbers[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+
+        }
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int numbers[] = { 2, 4, 6, 8, 10, 12, 14, 16 };
+        int key = 1;
+        System.out.println("index at:" + binarySearch(numbers, key));
+    }
+
+}
