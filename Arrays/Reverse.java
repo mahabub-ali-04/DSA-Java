4d68a9bbc1886e44abb9853485c1dd5b22018d18
Mahabub Ali <alimahabub977@gmail.com>
2026-01-31 15:14:53 +0530

    solved

---
+++ Arrays/Reverse.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class Reverse {
+    public static void reverseInArrays(int arr[]) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 1, 2, 4, 6, 3, 7, 8 };
+        reverseInArrays(arr);
+
+        // print
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+
+}
