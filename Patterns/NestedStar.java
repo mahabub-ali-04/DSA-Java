06bbe86d12f541519cf779728a70bbb51ffd5133
Mahabub Ali <alimahabub977@gmail.com>
2026-02-01 10:39:21 +0530

    solved

---
+++ Patterns/NestedStar.java
@@ -0,0 +1,22 @@
+package Patterns;
+import java.util.*;
+public class NestedStar {
+    public static void nestedLoopStar(int n){
+        // outer loop
+        for(int i =1; i<=n; i++){
+            //inner loop
+            for(int j =1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n:");
+        int n = sc.nextInt();
+        nestedLoopStar(n);
+    }
+    
+}
