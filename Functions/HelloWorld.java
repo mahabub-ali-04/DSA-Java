4ce6f2b234be78ce9c68859f5e5f6594ca692c20
Mahabub Ali <alimahabub977@gmail.com>
2026-02-02 11:21:05 +0530

    hello world

---
+++ Functions/HelloWorld.java
@@ -0,0 +1,12 @@
+package Functions;
+
+public class HelloWorld {
+    public static void hell0World() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String args[]) {
+        hell0World();
+
+    }
+}
