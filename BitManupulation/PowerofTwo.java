6fcf9c736e97d2d24477843f2d368a1ab83622cc
Mahabub Ali <alimahabub977@gmail.com>
2026-02-03 10:40:01 +0530

    solved

---
+++ BitManupulation/PowerofTwo.java
@@ -0,0 +1,16 @@
+package BitManupulation;
+
+public class PowerofTwo {
+    public static boolean powerOfTwo(int n) {
+        if ((n & (n - 1)) == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(powerOfTwo(3));
+
+    }
+
+}
