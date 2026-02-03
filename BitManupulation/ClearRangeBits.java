60094a3cc4c7772bb99fe32d67810cef7bdc416b
Mahabub Ali <alimahabub977@gmail.com>
2026-02-03 10:25:53 +0530

    solved

---
+++ BitManupulation/ClearRangeBits.java
@@ -0,0 +1,15 @@
+package BitManupulation;
+
+public class ClearRangeBits {
+    public static int clearRangeOfBits(int n, int i, int j) {
+        int a = ((~0) << j + 1);
+        int b = (1 << i) - 1;
+        int bitMask = a | b;
+        return n & bitMask;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(clearRangeOfBits(15, 1, 2));
+    }
+
+}
