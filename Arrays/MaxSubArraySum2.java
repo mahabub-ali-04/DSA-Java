1781e3124eb3b3d800ce6c8eae789c79d01ead88
Mahabub Ali <alimahabub977@gmail.com>
2026-02-05 10:45:42 +0530

    solved

---
+++ Arrays/MaxSubArraySum2.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class MaxSubArraySum2 {
+    public static void kadanes(int arr[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            // calculate sum
+            currSum += arr[i];
+            if (currSum < 0) {
+                currSum = 0;
+            }
+            // compare
+            maxSum = Math.max(maxSum, currSum);
+        }
+        System.out.println("max sum is: " + maxSum);
+
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+        kadanes(arr);
+    }
+
+}
