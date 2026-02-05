b5b09effaba59b639e53eb252cea2456fa72a9cd
Mahabub Ali <alimahabub977@gmail.com>
2026-02-05 11:06:00 +0530

    kadanes

---
+++ interview-questions/Array/Kadanes.java
@@ -0,0 +1,24 @@
+
+public class Kadanes {
+    public static void maxSubarraySum(int arr[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            // calculate sum
+            currSum += arr[i];
+            if (currSum < 0) {
+                currSum = 0;
+            }
+            // compare
+            maxSum = Math.max(maxSum, currSum);
+        }
+        System.out.println("max subarray sum is: " + maxSum);
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+        maxSubarraySum(arr);
+    }
+
+}
