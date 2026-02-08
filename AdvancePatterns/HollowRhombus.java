f22414bce90f8c6b9efe023cd99b3028c012feb1
Mahabub Ali <alimahabub977@gmail.com>
2026-02-08 21:45:39 +0530

    hollow rhombus pattern

---
+++ AdvancePatterns/HollowRhombus.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class HollowRhombus {
+    public static void hollowRhombus(int n) {
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                // boundry
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n:");
+        int n = sc.nextInt();
+        hollowRhombus(n);
+    }
+
+}
