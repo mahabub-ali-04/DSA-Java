c8ad8bb40d0ce93fc28da6c9c8be7f9e1ca55182
Mahabub Ali <alimahabub977@gmail.com>
2026-02-08 21:33:35 +0530

    solid rhombus patterns

---
+++ AdvancePatterns/SolidRhombus.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class SolidRhombus {
+    public static void solidRhombus(int n) {
+        for (int i = 1; i <= n; i++) {
+            // space
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // start
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        solidRhombus(n);
+    }
+
+}
