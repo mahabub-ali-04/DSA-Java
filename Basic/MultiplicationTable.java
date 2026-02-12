5091c02ce14e48c7c6dcad70984f1ed830ad9c54
Mahabub Ali <alimahabub977@gmail.com>
2026-02-12 11:41:46 +0530

    added multiplication table

---
+++ Basic/MultiplicationTable.java
@@ -0,0 +1,15 @@
+package Basic;
+
+import java.util.*;
+
+public class MultiplicationTable {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter n:");
+        int n = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + "*" + i + "= " + n * i);
+        }
+    }
+
+}
