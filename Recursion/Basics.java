d9fc3c9fa7cb0682fdb156a540d3f741e1a42f99
Mahabub Ali <alimahabub977@gmail.com>
2026-02-19 12:22:07 +0530

    fibonacci number

--- Recursion/Basics.java
+++ Recursion/Basics.java
@@ -41,12 +41,24 @@ public class Basics {
         return Sn;
     }
 
+    // print nth fibonacci number
+    public static int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int fnm1 = fib(n - 1);
+        int fnm2 = fib(n - 2);
+        int fn = fnm1 + fnm2;
+        return fn;
+    }
+
     public static void main(String args[]) {
         int n = 5;
         // printDec(n);
         // printIncreasing(n);
         // System.out.println(factorial(n));
-        System.out.println(sumNaturalNum(n));
+        // System.out.println(sumNaturalNum(n));
+        System.out.println(fib(n));
     }
 
 }
