2611fc3d88f907849e45b85b0deedb82b593c107
Mahabub Ali <alimahabub977@gmail.com>
2026-02-19 11:42:04 +0530

    sum of n natural number

--- Recursion/Basics.java
+++ Recursion/Basics.java
@@ -22,7 +22,6 @@ public class Basics {
     }
 
     // print factorial of a number
-
     public static int factorial(int n) {
         if (n == 0) {
             return 1;
@@ -32,11 +31,22 @@ public class Basics {
         return fn;
     }
 
+    // print sum of first n natural number
+    public static int sumNaturalNum(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int Snm1 = sumNaturalNum(n - 1);
+        int Sn = n + Snm1;
+        return Sn;
+    }
+
     public static void main(String args[]) {
         int n = 5;
         // printDec(n);
         // printIncreasing(n);
-        System.out.println(factorial(n));
+        // System.out.println(factorial(n));
+        System.out.println(sumNaturalNum(n));
     }
 
 }
