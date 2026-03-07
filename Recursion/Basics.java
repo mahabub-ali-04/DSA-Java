dcac058686b352e5c7675c3257a290ef9a8b5e02
Mahabub Ali <alimahabub977@gmail.com>
2026-03-07 13:38:44 +0530

    last occurence

--- Recursion/Basics.java
+++ Recursion/Basics.java
@@ -74,16 +74,29 @@ public class Basics {
         return firstOccurence(arr, key, i + 1);
     }
 
+    // WAF to find the last occurence of an element in an array
+    public static int lastOccurence(int arr[], int key, int i) {
+        if (i == arr.length) {
+            return -1;
+        }
+        int isFound = lastOccurence(arr, key, i + 1);
+        if (isFound == -1 && arr[i] == key) {
+            return i;
+        }
+        return isFound;
+    }
+
     public static void main(String args[]) {
         int n = 5;
-        int arr[] = { 1, 2, 3, 4, 5 };
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 5 };
         // printDec(n);
         // printIncreasing(n);
         // System.out.println(factorial(n));
         // System.out.println(sumNaturalNum(n));
         // System.out.println(fib(n));
         // System.out.println(isSorted(arr, 0));
-        System.out.println(firstOccurence(arr, 5, 0));
+        // System.out.println(firstOccurence(arr, 5, 0));
+        System.out.println(lastOccurence(arr, 5, 0));
     }
 
 }
