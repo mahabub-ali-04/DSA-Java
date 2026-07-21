7275b7d8fb5365249982bc2fc69b04382a246452
Mahabub Ali <alimahabub977@gmail.com>
2026-07-21 10:40:49 +0530

    Count Consonant

---
+++ String-Problem/Basic-Problems/CountConsonants.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class CountConsonants {
+    public static int countConsonants(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch != 'a' &&
+                    ch != 'e' &&
+                    ch != 'i' &&
+                    ch != 'o' &&
+                    ch != 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter String:");
+        String str = sc.nextLine();
+        System.out.println(countConsonants(str));
+    }
+
+}
