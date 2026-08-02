9dd8a8c5a03b19d55172a244ab7638263ab4ff43
Mahabub Ali <alimahabub977@gmail.com>
2026-08-02 12:03:56 +0530

    Operations

---
+++ ArrayList/Operations.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;;
+
+public class Operations {
+    public static void main(String args[]) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        // Add Element
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+
+        System.out.println(numbers);
+        //Get Element
+        // System.out.println(numbers.get(2));
+
+        // Remove Element
+        // numbers.remove(2);
+        // System.out.println(numbers);
+
+        //Set Element
+        // numbers.set(2,50);
+        // System.out.println(numbers);
+
+        //Contains Element
+
+        System.out.println(numbers.contains(40));
+    }
+
+}
